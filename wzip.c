ed14a7ed264bc104fdbbbf67a77a31464f3e6c15
Christian561 <chrislong561@gmail.com>
2023-09-28 19:15:49 -0700

    updated wzip to concat files before compressing

--- wzip.c
+++ wzip.c
@@ -63,39 +63,45 @@ int main(int argc, char *argv[]) {
     }
 
     // If files are provided, read from them.
+    char *buffer = NULL;
+    size_t bufferLength = 0; // keep track of the total length of buffer.
     for (int i = 1; i < argc; i++) {
         FILE *fptr = fopen(argv[i], "r");
         if (fptr == NULL) {
             printf("wzip: cannot open file\n");
             exit(1);
         }
-        //if char = char2 then ++, if char++ = char2++ then ++,...
-        
-        char *buffer = NULL;
-        size_t bufsize = 0;
-        size_t charactersRead;
-        //read each line
-        while ((charactersRead = getline(&buffer, &bufsize, fptr)) != -1) {
-            int k = 1; //counter for repeating characters
-            //loop over each char in the line
-            for (int m = 0; m < charactersRead; ++m) {
-                if (buffer[m] == buffer[m + 1]) {
-                    k++;
-                }
-                else{
-                    //write counter to stdout as binary
-                    fwrite(&k, sizeof(int), 1, stdout);
-                    //write char stdout
-                    fprintf(stdout, "%c",buffer[m]);
-                    k = 1; //reset counter
-                }
-
-            }
-        }
-        if (buffer) {
-            free(buffer);
+        //add each char into the buffer
+        int readCharacter;
+        while ((readCharacter = fgetc(fptr)) != EOF) {
+            buffer = realloc(buffer, bufferLength + 1); // add one byte for the new character
+            buffer[bufferLength] = (char)readCharacter; // append the character to buffer.
+            bufferLength++;
         }
         fclose(fptr);
+    }
+    
+    if (buffer != NULL) {
+        buffer[bufferLength] = '\0'; // null-terminate the buffer.
+    }
+    //process buffer
+    int k = 1; //counter for repeating characters
+    //loop over each char in the line
+    for (int m = 0; m < bufferLength; ++m) {
+        if (buffer[m] == buffer[m + 1]) {
+            k++;
+        }
+        else{
+            //write counter to stdout as binary
+            fwrite(&k, sizeof(int), 1, stdout);
+            //write char stdout
+            fprintf(stdout, "%c",buffer[m]);
+            k = 1; //reset counter
+        }
+
+    }
+    if (buffer != NULL) {
+        free(buffer); // don't forget to free the buffer when done.
     }
     return 0;
 }
